e82eed22abe4d35cad253b37a0cc0354d3ef04df
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2026-09-14 15:45:18 -0300

    fix en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/cine-frontend-cliente">
       <div className="flex min-h-screen flex-col bg-gray-950">
         <Navbar />
         <div className="flex-1">
